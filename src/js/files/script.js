894239ab01cbdb8c1143a5fc44b387e793775276
Maks <maks.pronevich@gmail.com>
2022-06-27 15:44:19 +0300

    08

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,22 @@
 import { isMobile } from './functions.js';
 // Подключение списка активных модулей
 import { flsModules } from './modules.js';
+
+const blocks = document.querySelectorAll('section');
+// window.getComputedStyle(el).backgroundColor;
+
+document.addEventListener('watcherCallback', function (e) {
+	const entry = e.detail.entry;
+	const targetElement = entry.target;
+	const header = document.querySelector('header');
+
+	blocks.forEach(block => {
+		if (targetElement.classList.contains('_watcher-view')) {
+			block.classList.add('bg-green');
+			header.classList.add('bg-green');
+		} else {
+			block.classList.remove('bg-green');
+			header.classList.remove('bg-green');
+		}
+	});
+});
