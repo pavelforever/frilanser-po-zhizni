3ddf08acd4d893eb87d98a1898249c222a352fb5
Maks <maks.pronevich@gmail.com>
2022-07-14 08:35:56 +0300

    18

--- src/js/libs/dynamic_adapt.js
+++ src/js/libs/dynamic_adapt.js
@@ -4,7 +4,7 @@
 // Andrikanych Yevhen 2020
 // https://www.youtube.com/c/freelancerlifestyle
 
-"use strict";
+'use strict';
 function DynamicAdapt(type) {
 	this.type = type;
 }
@@ -12,28 +12,32 @@ DynamicAdapt.prototype.init = function () {
 	const _this = this;
 	// массив объектов
 	this.оbjects = [];
-	this.daClassname = "_dynamic_adapt_";
+	this.daClassname = '_dynamic_adapt_';
 	// массив DOM-элементов
-	this.nodes = document.querySelectorAll("[data-da]");
+	this.nodes = document.querySelectorAll('[data-da]');
 	// наполнение оbjects объктами
 	for (let i = 0; i < this.nodes.length; i++) {
 		const node = this.nodes[i];
 		const data = node.dataset.da.trim();
-		const dataArray = data.split(",");
+		const dataArray = data.split(',');
 		const оbject = {};
 		оbject.element = node;
 		оbject.parent = node.parentNode;
 		оbject.destination = document.querySelector(dataArray[0].trim());
-		оbject.breakpoint = dataArray[1] ? dataArray[1].trim() : "767";
-		оbject.place = dataArray[2] ? dataArray[2].trim() : "last";
+		оbject.breakpoint = dataArray[1] ? dataArray[1].trim() : '767';
+		оbject.place = dataArray[2] ? dataArray[2].trim() : 'last';
 		оbject.index = this.indexInParent(оbject.parent, оbject.element);
 		this.оbjects.push(оbject);
 	}
 	this.arraySort(this.оbjects);
 	// массив уникальных медиа-запросов
-	this.mediaQueries = Array.prototype.map.call(this.оbjects, function (item) {
-		return '(' + this.type + "-width: " + item.breakpoint + "px)," + item.breakpoint;
-	}, this);
+	this.mediaQueries = Array.prototype.map.call(
+		this.оbjects,
+		function (item) {
+			return '(' + this.type + '-width: ' + item.breakpoint + 'px),' + item.breakpoint;
+		},
+		this
+	);
 	this.mediaQueries = Array.prototype.filter.call(this.mediaQueries, function (item, index, self) {
 		return Array.prototype.indexOf.call(self, item) === index;
 	});
@@ -83,7 +87,7 @@ DynamicAdapt.prototype.moveTo = function (place, element, destination) {
 		return;
 	}
 	destination.children[place].insertAdjacentElement('beforebegin', element);
-}
+};
 // Функция возврата
 DynamicAdapt.prototype.moveBack = function (parent, element, index) {
 	element.classList.remove(this.daClassname);
@@ -92,28 +96,28 @@ DynamicAdapt.prototype.moveBack = function (parent, element, index) {
 	} else {
 		parent.insertAdjacentElement('beforeend', element);
 	}
-}
+};
 // Функция получения индекса внутри родителя
 DynamicAdapt.prototype.indexInParent = function (parent, element) {
 	const array = Array.prototype.slice.call(parent.children);
 	return Array.prototype.indexOf.call(array, element);
 };
-// Функция сортировки массива по breakpoint и place 
+// Функция сортировки массива по breakpoint и place
 // по возрастанию для this.type = min
 // по убыванию для this.type = max
 DynamicAdapt.prototype.arraySort = function (arr) {
-	if (this.type === "min") {
+	if (this.type === 'min') {
 		Array.prototype.sort.call(arr, function (a, b) {
 			if (a.breakpoint === b.breakpoint) {
 				if (a.place === b.place) {
 					return 0;
 				}
 
-				if (a.place === "first" || b.place === "last") {
+				if (a.place === 'first' || b.place === 'last') {
 					return -1;
 				}
 
-				if (a.place === "last" || b.place === "first") {
+				if (a.place === 'last' || b.place === 'first') {
 					return 1;
 				}
 
@@ -129,11 +133,11 @@ DynamicAdapt.prototype.arraySort = function (arr) {
 					return 0;
 				}
 
-				if (a.place === "first" || b.place === "last") {
+				if (a.place === 'first' || b.place === 'last') {
 					return 1;
 				}
 
-				if (a.place === "last" || b.place === "first") {
+				if (a.place === 'last' || b.place === 'first') {
 					return -1;
 				}
 
@@ -145,5 +149,5 @@ DynamicAdapt.prototype.arraySort = function (arr) {
 		return;
 	}
 };
-const da = new DynamicAdapt("max");
+const da = new DynamicAdapt('max');
 da.init();
